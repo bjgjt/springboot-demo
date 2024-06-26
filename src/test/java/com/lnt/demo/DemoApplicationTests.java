04e139ac1ed41c9e2af48eb9b9932282e354633e
tuanln <tuanln@vnpt.vn>
2024-06-26 22:02:07 +0700

    first commit

---
+++ src/test/java/com/lnt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
